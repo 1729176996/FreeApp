99e98985069b178a43369deebb3bdd864aa8c2fc
1729176996 <1729176996@qq.com>
2019-12-26 10:23:13 +0800

    555

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var vm,loading;
+$(function(){
+    FastClick.attach(document.body);
+    vm = new Vue({
+        el: "#main",
+        data:{
+			
+        },
+        mounted:function(){
+            
+        },
+        methods:{
+			sub:function(){
+				
+            }
+        }
+    })
+});
